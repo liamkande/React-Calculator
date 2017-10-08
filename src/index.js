e6fd1970b618441793e37beefce5ac4aff65036a
Ansoumane Kande <liamkande@Ansoumanes-MBP.home>
2017-10-07 22:52:48 -0400

    React-Calculator-v1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Calculator} from './App.js'
+
+
+ReactDOM.render(
+  <div id="wrapper">
+    <Calculator/>
+  </div>,
+  document.getElementById('root')
+)
